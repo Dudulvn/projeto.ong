e87750a16799dfa424b7e69c9f348fad16e2e767
Dudulvn <eduardo.lvnogueira@gmail.com>
2026-05-31 18:34:49 -0300

    Update settings.py

--- arca_protecao/settings.py
+++ arca_protecao/settings.py
@@ -28,13 +28,9 @@ SECRET_KEY = 'django-insecure-us3g&km*h5&#$(h!7gzj#o-v4mvt$_en2nrofg^&*jb#$^7_hi
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '.vercel.app',
-    'localhost',
     '127.0.0.1',
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://*.vercel.app',
+    'localhost',
+    '.onrender.com',
 ]
 
 
